25c707a77b16c8f93d1b7df7044d64d3bd89d0de
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-19 14:29:49 +0530

    [Kavyasree J ganesh] Employee wage practice problem - full time static variable

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,6 +1,8 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static int WAGE_PER_HOUR = 20;
+    static int FULL_DAY_HOUR = 8
     public static void main(String[] args) {
         System.out.println("Welcome to Employee + Wage Computation Program");
 
