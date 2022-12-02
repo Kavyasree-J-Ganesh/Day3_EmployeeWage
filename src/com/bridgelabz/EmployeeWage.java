cb8b2462f6e459980179d1c6bddecabd169372cd
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-03 02:02:38 +0530

    [Kavyasree J Ganesh] Employee wage list added with option to add in list

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -10,7 +10,22 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee + Wage Computation Program");
     }
 
+    static void addToList(CompanyEmpWage emp){
+       companyList.add(emp);
+    }
 
+    static void displayCompanyList(){
+        System.out.println("Companies");
+        System.out.println("__________________");
+        for(int i=0;i<companyList.size();i++){
+            System.out.println("Company : " + companyList.get(i).companyName);
+            System.out.println("__________________");
+            System.out.println("Company : " + companyList.get(i).companyName);
+            System.out.println("Wage per hour : " + companyList.get(i).wagePerHour);
+            System.out.println("total wage : " + companyList.get(i).totalWage);
+            System.out.println("");
+        }
+    }
 
     public static void main(String[] args) {
         printWelcome();
@@ -18,6 +33,10 @@ public class EmployeeWage {
         infosys.getTotalWage();
         CompanyEmpWage tcs = new CompanyEmpWage("tcs",900,800, 22);
         tcs.getTotalWage();
+        addToList(tcs);
+        addToList(infosys);
+
+        displayCompanyList();
     }
 }
 
